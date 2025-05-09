56b3559dd72544e08e6be4ddb9eacbff463d8966
Renat Mingazov <mingazofff@gmail.com>
2025-05-09 17:07:18 +0300

    Пустой шаблон Spring Initializr

---
+++ src/main/java/ru/mingazoff/userAndSubscriptionService/UserAndSubscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.mingazoff.userAndSubscriptionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAndSubscriptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAndSubscriptionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mingazoff/userAndSubscriptionService/UserAndSubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mingazoff.userAndSubscriptionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAndSubscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
